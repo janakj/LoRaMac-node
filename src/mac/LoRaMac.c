a65e954473ab1ee17d923b53f11813d061acb6b9
Jan Janak <jan@janakj.org>
2022-06-15 02:42:50 -0400

    Reactivate radio for continuous RX when starting in class C

--- src/mac/LoRaMac.c
+++ src/mac/LoRaMac.c
@@ -3821,6 +3821,7 @@ LoRaMacStatus_t LoRaMacInitialization( LoRaMacPrimitives_t* primitives, LoRaMacC
 LoRaMacStatus_t LoRaMacStart( void )
 {
     MacCtx.MacState = LORAMAC_IDLE;
+    UpdateRxSlotIdleState();
     return LORAMAC_STATUS_OK;
 }
 
